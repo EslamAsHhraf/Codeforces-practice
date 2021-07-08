9e948bf6b65c2a2dc55dbd8791f78ef847892e6a
EslamAsHhraf <eslamashraf.12342@gmail.com>
2021-07-08 12:15:47 +0200

    Hungry Sequence

---
+++ B/Hungry Sequence.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+#include <stdio.h>
+//# include<string>
+# include<string>
+# include<string.h>
+#include <algorithm>
+# include<vector>
+# include<map>
+# include<set>
+//# include<list>
+//#include <numeric> 
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 10000000 - n; i < 10000000; i++) cout << i << " ";
+	
+}
