1f2797755ec3552558f1e4fe7bedb4ae062a8dbe
EslamAsHhraf <eslamashraf.12342@gmail.com>
2021-11-05 14:19:59 +0200

    Birds

--- acmASCIS Level 0 Training '22 Mirror/Week 1 Sheet/(A) U.A. Scoring.cpp
+++ acmASCIS Level 0 Training '22 Mirror/Week 1 Sheet/(A) U.A. Scoring.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main()
 {
 	ios_base::sync_with_stdio(0), cin.tie(0); cout.tie(0);
-	ll n;
+	ll n;;
 	cin >> n;
-	if (n % 2 != 0 || n == 2)
-		cout << "NO";
-	else
-		cout << "YES";
+	cout << (n)*(n + 1);
 }
