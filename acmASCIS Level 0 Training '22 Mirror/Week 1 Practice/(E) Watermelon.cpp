8414df85ba262de8d38142c756ac7e620ed60b1b
EslamAsHhraf <eslamashraf.12342@gmail.com>
2021-11-05 14:29:02 +0200

    Never Give UP

---
+++ acmASCIS Level 0 Training '22 Mirror/Week 1 Practice/(E) Watermelon.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0); cout.tie(0);
+	ll n;
+	cin >> n;
+	if (n % 2 != 0 || n==2)
+		cout << "NO";
+	else
+		cout << "YES";
+}
