a0f7925bceef49227e01ad0f2b04fe2773cab7a5
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-02-17 01:08:28 +0200

    empire state of mind

---
+++ B/415B - Mashmokh and Tokens.cpp
@@ -0,0 +1,14 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	double n, a, b, x;
+	cin >> n >> a >> b;
+	while (n--)
+	{
+		cin >> x;
+		cout << (ll)floor(x - ((b / a)*floor(x*a / b))) << endl;
+	}
+}
