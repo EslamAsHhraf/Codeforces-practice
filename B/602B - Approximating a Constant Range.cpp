75a2c793d37634c1807b80297d0398eb81472d4d
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-02-26 23:03:01 +0200

    Hns3d

---
+++ B/602B - Approximating a Constant Range.cpp
@@ -0,0 +1,32 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ll n, a, Max = 0, Min = INT_MAX;
+    cin >> n;
+    vector<ll>v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    ll j = 0, ans = 0, diff;
+    multiset<ll>mySet;
+    for (int i = 0; i < n; i++)
+    {
+        mySet.insert(v[i]);
+        Max = (*mySet.rbegin());
+        Min = (*mySet.begin());
+        diff = Max - Min;
+        while (diff > 1)
+        {
+            mySet.erase(mySet.find(v[j]));
+            Max = (*mySet.rbegin());
+            Min = (*mySet.begin());
+            diff = Max - Min;
+            j++;
+        }
+        ans = max(i - j, ans);
+    }
+    cout << ans + 1;
+}
---
+++ B/614B - Gena's Code.cpp
@@ -0,0 +1,29 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    ll n, cnt = 0;
+    string s, unBeautiful = "1";
+    cin >> n;
+    for (ll i = 0; i < n; i++) 
+    {
+        cin >> s;
+        if (s == "0") 
+        {
+            cout << 0;
+            return 0;
+        }
+        bool beautiful =( s[0] == '1');
+        for (ll j = 1; j < s.size(); j++)
+            beautiful &= (s[j] == '0');
+        if (beautiful)
+            cnt += s.size() - 1;
+        else
+            unBeautiful = s;
+    }
+    cout << unBeautiful;
+    for (ll i = 0; i < cnt; i++) 
+        cout << 0;
+    return 0;
+}
