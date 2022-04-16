9804bd712124080ca626efe40fc8247f408defb2
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-04-16 20:08:00 +0200

    soma el3asher

---
+++ B/621B - Wet Shark and Bishops.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll x, y;
+map<ll, ll>dig1;
+map<ll, ll>dig2;
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ll n, count = 0;
+	cin >> n;
+	for(int i=0;i<n;i++)
+	{
+		cin >> x >> y;
+		count += dig1[x + y];
+		count += dig2[x - y];
+		dig1[x+y]++;
+		dig2[x-y]++;
+	}
+	cout << count;
+
+}
