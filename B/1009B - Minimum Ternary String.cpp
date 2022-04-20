252983ae180b8f8023543c996f555a42415cd83e
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-04-20 18:27:18 +0200

    Walter White

---
+++ B/1009B - Minimum Ternary String.cpp
@@ -0,0 +1,34 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll a[4];
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    string s;
+    cin >> s;
+    int n = s.size();
+    int k = n;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (s[i] == '0')
+            a[0]++;
+        if (s[i] == '2')
+        {
+            k = i;
+            a[2]++;
+        }
+        if (s[i] == '1')
+            a[1]++;
+    }
+    for (int i = 0; i < k; i++)
+    {
+        if (s[i] == '0')
+            cout << "0";
+    }
+    for (int i = 0; i < a[1]; i++)
+        cout << "1";
+    for (int i = k; i < n; i++)
+        if (s[i] != '1')
+            cout << s[i];
+
+}
---
+++ B/535B - Tavas and SaDDas.cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int indx = 0;
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    for (int i = 0;i<s.size();i++)
+    {
+        if (s[i] == '4')
+            indx += pow(2,i) * 1;
+        else
+            indx += pow(2, i) * 2;
+    }
+    
+    cout << indx;
+}
