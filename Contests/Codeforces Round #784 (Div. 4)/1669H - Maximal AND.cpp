875332b5a308238cf6a8e03f5abd1aa1069fe827
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-04-24 18:13:50 +0200

    bakal

---
+++ Contests/Codeforces Round #784 (Div. 4)/1669H - Maximal AND.cpp
@@ -0,0 +1,36 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll n, k;
+void solve()
+{
+    ll  x, p, sum = 0;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (ll i = 30; i >= 0; i--) {
+        p = 0;
+        x = pow(2, i);
+        for (ll j = 0; j < n; j++) {
+            if ((a[j] & x) == 0) {
+                p++;
+            }
+        }
+        if (p <= k) {
+            k -= p;
+            sum = sum + x;
+        }
+    }
+    cout << sum << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ll t;
+	cin >> t;
+	while (t--)
+		solve();
+	
+}
