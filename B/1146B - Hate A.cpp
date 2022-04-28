631affc62359a4ee76e1c603eead18f0cd8bf4b2
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-04-28 17:41:11 +0200

    legcyyyyyyyy

---
+++ B/1146B - Hate A.cpp
@@ -0,0 +1,44 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+	string s1, s2;
+	cin >> s1 ;
+	int k=0 ;
+	for (int i = s1.size()-1; i>= 0.; i--) {
+		if (s1[i] == 'a')
+		{
+			k = i;
+			break;
+		}
+	}
+	string temp = s1;
+	s2 = s1.substr(k + 1,s1.size());
+	s1.erase(k+1, s1.size());
+	s1.erase(remove(s1.begin(), s1.end(), 'a'),s1.end());
+	if (s1.size() > s2.size())
+	{
+		cout << ":(";
+		return;
+	}
+	s1 += s2;
+	s2= s1.substr(s1.size()/2, s1.size());
+	s1.erase(s1.size() / 2, s1.size());
+	if (s1 == s2)
+	{
+		temp.erase(temp.size() - s2.size(), temp.size());
+		cout << temp;
+	}
+	else
+		cout << ":(";
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ll t;
+	/*cin >> t;
+	while(t--)*/
+		solve();
+
+}
---
+++ FE-CU Training 2021-2022/FECU - Team Formation 2022 #2/(F) Hard Challenge.cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve()
+{
+	ll a, b;
+	cin >> a >> b;
+	if (a % b == 0)
+	{
+		cout << 0 << endl;
+		return;
+	}
+	ll temp = a / b;
+	cout << (temp+1) * b - a << endl;
+	
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ll t;
+	cin >> t;
+	while(t--)
+		solve();
+
+}
